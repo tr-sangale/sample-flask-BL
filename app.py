a7e63d8b9ea0ccde643f15c5c71434aa5d2bd857
tr-sangale <tejassangale2@gmail.com>
2022-08-17 02:30:12 +0530

    "first commit"

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+import time
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+
+@app.route("/times")
+def times():
+    t=time.localtime()
+    current_time = time.strftime("%H:%M:%S", t)
+    return current_time
+
+app.run(debug=True)
